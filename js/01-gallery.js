b409b5113bb51d7da90e192f29652dd474bd5232
AndrijKarmaza <andrij.karmaza@gmail.com>
2023-03-31 19:51:57 +0300

    homework is done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const marcupGallery = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
+<a class="gallery__link" href='${original}'>
+  <img
+    class="gallery__image qwer"
+    src='${preview}'
+    data-source='${original}'
+    alt='${description}'
+  />
+</a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', marcupGallery);
+gallery.addEventListener('click', onImageClick);
+
+let modal;
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;        
+    }
+
+    const imgLink = evt.target.dataset.source;
+    modal = basicLightbox.create(`<img src="${imgLink}">`);
+    modal.show();
+
+    document.addEventListener('keydown', onEscKeyPress);
+}
+
+function onEscKeyPress(evt) {
+    if (evt.code !== "Escape") {
+        return;
+    }
+
+    modal.close();
+    document.removeEventListener('keydown', onEscKeyPress);
+    
+}
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const marcupGallery = galleryItems.map(({preview, original, description}) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML('beforeend', marcupGallery);
+gallery.addEventListener('click', onImageClick);
+
+let modal = new SimpleLightbox('.gallery a', 
+    {captionsData: 'alt', captionDelay: 250, fadeSpeed: 250,
+    scrollZoom: false, disableRightClick: true});
+
+function onImageClick(evt) {
+    evt.preventDefault();
+
+    if (!evt.target.classList.contains('gallery__image')) {
+        return;        
+    }
+    
+    modal.on('show.simplelightbox');
+}
